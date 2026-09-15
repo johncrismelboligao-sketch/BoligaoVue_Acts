bed8fdfbb8aa8f867b1c3989f5c1439f21eb50a3
johncrismelboligao-sketch <johncrismelboligao@gmail.com>
2026-09-15 13:17:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      |
+      <RouterLink to="/about">About</RouterLink>
+      |
+      <RouterLink :to="{ name: 'Contact' }">Contact</RouterLink>
+     </nav>
+    <br>
+    <RouterView/>
+  </div>
+</template>
